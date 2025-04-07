45dd5d2879db09f569a9f2792ab230bb732ee974
Gayathri Krishnan <iamkgayu@gmail.com>
2025-04-07 16:48:07 +0100

    Added README file

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -3,7 +3,7 @@ import TodoItem from "./TodoItem";
 export default function TodoList({todos, setTodos}) {
   const sortedTodos = todos.slice().sort((a,b) => Number(a.done - b.done));
     return (
-      <div className={styles.list}> 
+      <div className={styles.list}>
       {sortedTodos.map((item) => (
        <TodoItem item={item} key={item.name} todos ={todos} setTodos={setTodos}/>
       ))}
